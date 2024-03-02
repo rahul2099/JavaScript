f6e70a91368c9576196b3f550370c2740ca17379
rahul2099 <148942897+rahul2099@users.noreply.github.com>
2024-03-02 15:48:11 +0530

    data types

---
+++ 02_dataTypes.js
@@ -0,0 +1,26 @@
+"use strict"; //treat all JS code as newer version
+// alert("hello") //we are using nodejs not browser
+
+console.log(3
+    + 3); // code readability is high
+
+console.log("hitesh")
+
+let name ="hitesh"
+let age =17
+let isLoggedIn = false
+let state;
+
+// number => 2 to power 53
+//bigint 
+// string =>""
+// boolean => true/false
+// null => standalone value
+// undefined =>
+// symbol =>unique
+
+
+// object 
+
+console.log(typeof null); //object
+console.log(typeof undefined); //undefined
