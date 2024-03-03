dbd36a22805a692c01a9083ea5975c1d7c1a9988
rahul2099 <148942897+rahul2099@users.noreply.github.com>
2024-03-03 19:52:05 +0530

    Comparison

---
+++ 04_comparison.js
@@ -0,0 +1,26 @@
+// console.log(2 > 1);
+// console.log(2 >=1);
+// console.log(2 <1);
+// console.log(2 ==1);
+// console.log(2 !=1);
+
+
+// console.log("2" > 1);
+// console.log("02" > 1);
+
+console.log(null > 0);
+console.log(null ==0);
+console.log(null >=0);
+
+console.log(undefined ==0 );
+console.log(undefined > 0 );
+console.log(undefined < 0 );
+// The reason is that an equality check == and comparisons ><>=
+// <= work differently
+// Comparisons covert null to a number , treating it as 0.
+// That's why(3) null >=0 is true and (!) null >0 is false
+
+// ===
+
+console.log("2" == 2);
+
