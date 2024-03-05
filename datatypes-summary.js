8bfc86d1fb54c0295631ce2ec5fea5d239a6f9c7
rahul2099 <148942897+rahul2099@users.noreply.github.com>
2024-03-05 10:28:11 +0530

    Stacks and Heap

--- datatypes-summary.js
+++ datatypes-summary.js
@@ -16,6 +16,8 @@ const id = Symbol('1234')
 const anotherId = Symbol('1234')
 
 console.log(id === anotherId);
+// In JavaScript, === is a comparison operator that checks the equality of two values without performing any type conversion
+// The == operator in JavaScript is the equality operator. It checks if two values are equal.
 
 // const bigNumber = 343534n
 // Reference (Non primitive)
@@ -33,3 +35,27 @@ const myFunction= function(){
 }
 
 console.log(typeof myFunction);
+
+// **********************************************************************
+// Memory type -Stack and Heap
+// Stack(Primitive),Heap(Non-Primitive) 
+// Non Primitive mein Original value ka reference milta hai
+let myYoutubename = "hiteshchaudharydotcom"
+
+let anothername = myYoutubename
+anothername = "chaiaurcode"
+
+console.log(myYoutubename);
+console.log(anothername);
+
+let userOne = {
+    email: "user@google.com",
+    upi: "user@ypl"
+}
+
+let userTwo = userOne
+
+userTwo.email = "hitesh@google.com"
+
+console.log(userOne.email);
+console.log(userTwo.email);
