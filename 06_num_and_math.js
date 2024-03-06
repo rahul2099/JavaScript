4dc3dbc0cffeb81c18b4431f16f9015c031a70ac
rahul2099 <148942897+rahul2099@users.noreply.github.com>
2024-03-06 08:36:44 +0530

    Math and Num

---
+++ 06_num_and_math.js
@@ -0,0 +1,39 @@
+const score = 400
+// console.log(score);
+const balance = new Number(100)
+
+// console.log(balance);
+
+// console.log(balance.toString().length);
+// console.log(balance.toFixed(1));
+
+const otherNumber = 34.3654
+
+// console.log(otherNumber.toPrecision(3));
+
+const hundreds = 1000000
+// console.log(hundreds.toLocaleString('en-IN'));
+
+
+// ****************************Maths************************************
+
+
+// console.log(Math);
+// console.log(Math.abs(-4));
+// console.log(Math.round(4.6));
+// console.log(Math.ceil(4.2));
+// console.log(Math.floor(4.9));
+// console.log(Math.min(4,5,78,5));
+// console.log(Math.max(6,7,9,5,3,86,8,34));
+
+console.log(Math.random());
+// Math.random hamesha 0 ya 1 number deta hai randomly
+
+console.log((Math.random()*10)+1);
+console.log(Math.floor(Math.random()*10) + 1);
+
+const min =10
+const max = 20
+
+console.log(Math.floor(Math.random()*(max - min + 1)) + min);
+
