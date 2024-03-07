94e3b32a96bcec7d9c60707dd08a09534b3fb6b7
rahul2099 <148942897+rahul2099@users.noreply.github.com>
2024-03-07 12:49:50 +0530

    DatesinJs

--- 07_datesinjs.js
+++ 07_datesinjs.js
@@ -1,12 +1,31 @@
 // Dates
 
 let myDate = new Date()
-console.log(myDate.toString());
-console.log(myDate.toISOString());
-console.log(myDate.toLocaleDateString());
-console.log(myDate.toLocaleString());
-console.log(myDate.toLocaleTimeString());
-console.log(myDate.toString());
-console.log(myDate.getTimezoneOffset());
-console.log(typeof myDate);
+// console.log(myDate.toString());
+// console.log(myDate.toISOString());
+// console.log(myDate.toLocaleDateString());
+// console.log(myDate.toLocaleString());
+// console.log(myDate.toLocaleTimeString());
+// console.log(myDate.toString());
+// console.log(myDate.getTimezoneOffset());
+// console.log(typeof myDate);
 
+// let myCreatedDate = new Date(2023,2,23,5,3)
+let myCreatedDate = new Date("01-14-2023")
+// console.log(myCreatedDate.toDateString());
+// console.log(myCreatedDate.toLocaleString());
+
+let myTimeStamp = Date.now()
+
+// console.log(myTimeStamp);
+// console.log(myCreatedDate.getTime());
+// console.log(Math.floor(Date.now()/1000));
+
+let newDate = new Date()
+console.log(newDate);
+console.log(newDate.getMonth());
+console.log(newDate.getDay());
+
+newDate.toLocaleString('default',{
+    weekday:"long", 
+})
