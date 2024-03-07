c0f6de4adf2dcb9bf380ed023c4258914e0f75d9
rahul2099 <148942897+rahul2099@users.noreply.github.com>
2024-03-07 12:27:23 +0530

    dates

--- 07_datesinjs.js
+++ 07_datesinjs.js
@@ -1,15 +1,12 @@
 // Dates
 
 let myDate = new Date()
-// console.log(myDate.toString());
-// console.log(myDate.toISOString());
-// console.log(myDate.toLocaleDateString());
-// console.log(myDate.toLocaleString());
-// console.log(myDate.toLocaleTimeString());
-// console.log(myDate.toString());
-// console.log(myDate.getTimezoneOffset());
-// console.log(typeof myDate);
+console.log(myDate.toString());
+console.log(myDate.toISOString());
+console.log(myDate.toLocaleDateString());
+console.log(myDate.toLocaleString());
+console.log(myDate.toLocaleTimeString());
+console.log(myDate.toString());
+console.log(myDate.getTimezoneOffset());
+console.log(typeof myDate);
 
-let myCreatedDate = new Date(2023, 0 , 23)
-console.log(myCreatedDate.toDateString());
-8:09
