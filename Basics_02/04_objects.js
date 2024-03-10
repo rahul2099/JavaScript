0906e5a74245622ec4b2cd89a15940b16435f885
rahul2099 <148942897+rahul2099@users.noreply.github.com>
2024-03-10 14:15:33 +0530

    objecs and apis

--- Basics_02/04_objects.js
+++ Basics_02/04_objects.js
@@ -26,7 +26,7 @@ const obj4 = {5: "a", 6: "b"}
 
 // const obj3 = Object.assign({},obj1,obj2,obj4)
 const obj3 = {...obj1, ...obj2}
-console.log(obj3);
+// console.log(obj3);
 
 const users = [
     {
@@ -48,10 +48,65 @@ const users = [
 ]
 
 users[1].email
-console.log(tinderUser);
+// console.log(tinderUser);
 
-console.log(Object.keys(tinderUser));
-console.log(Object.values(tinderUser));
-console.log(Object.entries(tinderUser));
+// console.log(Object.keys(tinderUser));
+// console.log(Object.values(tinderUser));
+// console.log(Object.entries(tinderUser));
 
-console.log(tinderUser.hasOwnProperty('isLoggedIn'));
+// console.log(tinderUser.hasOwnProperty('isLoggedIn'));
+
+// Destructure
+const course = {
+    coursename: "js in hindi",
+    price: "999",
+    courseInstructor:"hitesh",
+}
+
+// course.courseInstructor
+
+const {courseInstructor:instructor} = course
+
+console.log(instructor);
+
+// {
+//     "name":"hitesh",
+//     "coursename": "js in hindi",
+//     "price": "free"
+// }
+
+[
+    {},
+    {},
+    {}
+]
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
